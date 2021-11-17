65c5a0598c980a096007b01c35708416886137f0
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2021-11-17 16:35:04 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="definetti",
-    version="0.0.1",
+    version="0.0.2",
     description="Lottery like mechanisms",
     long_description=README,
     long_description_content_type="text/markdown",
